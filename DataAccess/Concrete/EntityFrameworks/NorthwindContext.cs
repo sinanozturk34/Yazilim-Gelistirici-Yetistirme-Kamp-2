d418231c9f62e37110f6befbfc1e318ac536cf63
Sinan Ozturk <sinansinan_@outlook.com.tr>
2023-11-22 15:51:37 +0300

    9.gün

--- DataAccess/Concrete/EntityFrameworks/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFrameworks/NorthwindContext.cs
@@ -17,6 +17,6 @@ namespace DataAccess.Concrete.EntityFrameworks
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Customer> Customers { get; set; }
-       // public DbSet<Order> Orders { get; set; }
+        public DbSet<Order> Orders { get; set; }
     }
 }
